535ba0f9e49f8f41b979ee6528f482321cd51547
Jason Cannon <jason_cannon@outlook.com>
2022-12-01 15:00:44 -0500

    Added day 1 solutions

---
+++ day_01/day_01b.py
@@ -0,0 +1,8 @@
+import sys
+
+n, Elves = 1, [0]
+for line in map(lambda x : x.strip(), sys.stdin.readlines()):
+    if line: Elves[n - 1] += int(line)
+    else: n, Elves = n + 1, Elves + [0]
+
+print(sum(sorted(Elves)[-3:]))
