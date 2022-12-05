9d355ceb6d30323a5da99f3f0c77228158f3a538
Jason Cannon <jason_cannon@outlook.com>
2022-12-05 01:32:41 -0500

    Added day 5 solutions

---
+++ day_05/day_05b.py
@@ -0,0 +1,21 @@
+import sys
+
+Lines = list(map(lambda x: x.strip('\n'), sys.stdin.readlines()))
+idx = Lines.index('')
+
+N = int(Lines[idx-1].split()[-1])
+Stacks = [[] for _ in range(N)]
+
+for i in range(idx-2, -1, -1):
+    for j in range(N):
+        val = Lines[i][1 + 4*j]
+        if val != ' ':
+            Stacks[j].append(val)
+
+for i in range(idx+1, len(Lines)):
+    cmd = Lines[i].split()
+    amt, fr, to = int(cmd[1]), int(cmd[3]) - 1, int(cmd[5]) - 1
+    Stacks[to] +=  Stacks[fr][-amt:]
+    del Stacks[fr][-amt:]
+
+print(''.join(S[-1] if S else '' for S in Stacks))
