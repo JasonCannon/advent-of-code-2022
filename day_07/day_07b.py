df06de44f3241cb978d6a1a4edef1808e773295f
Jason Cannon <jason_cannon@outlook.com>
2022-12-07 20:01:34 -0500

    Added day 7 solutions

---
+++ day_07/day_07b.py
@@ -0,0 +1,30 @@
+import sys
+from collections import defaultdict
+from functools import lru_cache
+
+Lines = list(map(lambda x: x.strip().split(), sys.stdin.readlines()))
+Dirs, path, idx = defaultdict(set), "", 0
+
+while idx < len(Lines):
+    if Lines[idx][1] == "cd":
+        if Lines[idx][2] == '/': path = '/'
+        elif Lines[idx][2] == "..": path = path[:path[:-1].rindex('/')+1]
+        else: path += f"{Lines[idx][2]}/"
+        idx += 1
+    elif Lines[idx][1] == "ls":
+        S, idx = set(), idx + 1
+        while idx < len(Lines) and Lines[idx][0] != '$':
+            S.add((Lines[idx][1], Lines[idx][0]))
+            idx += 1
+        Dirs[path] = Dirs[path].union(S)
+
+@lru_cache(maxsize=None)
+def get_dir_size(s):
+    sz = 0
+    for k, v in Dirs[s]:
+        if v == "dir": sz += get_dir_size(s + f"{k}/")
+        else: sz += int(v)
+    return sz
+
+needed = get_dir_size("/") - 40000000
+print(sorted([get_dir_size(k) for k in Dirs if get_dir_size(k) >= needed])[0])
